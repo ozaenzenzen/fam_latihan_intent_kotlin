9132fa2095805e81ecc118651322415365bc4fa9
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-02-04 12:23:07 +0700

    latihan intent 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.myintentapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myintentapp"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
